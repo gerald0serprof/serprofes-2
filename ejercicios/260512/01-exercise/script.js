119dc803ad467c37354ea325560d95f75ddbe357
gerald0serprof <gerald.lima.587@alumni.serprofes.es>
2026-05-12 21:35:50 +0200

    Added exercise-01 cesta_compra and exercise-02 fichas_tecnicas

---
+++ ejercicios/260512/01-exercise/script.js
@@ -0,0 +1,27 @@
+//1. Creamos nuestra "Caja grande" (Array)
+let carrito = ["🍏 Manzana","🍌Plátano"];
+//2.Imprimir el carrito nada mas al cargar la página
+document.getElementById('pantalla-cesta').textContent = carrito.join(" - ");
+
+//3. La función para agregar cosas nuevas a la lista
+function añadirAlCarrito(){
+    //Atrapamos lo que escribe el usuario
+    let item = document.getElementById('nuevo-item').value;
+    // MAGIA DE ARRAYS: .push() mete el nuevo elemento al final de la lista
+    carrito.push(item);
+    //Volvemos a pintar la cesta para que se vea
+    document.getElementById('pantalla-cesta').textContent = carrito.join(" - ");
+    //Limpiamos el input
+    document.getElementById('nuevo-item').value = "";
+}
+
+//4. La función para borrar cosas ya existentes a la lista
+
+function borrarElemento(item){
+    let posicion = document.getElementById(item).value;
+    carrito.pop();
+
+    //Actualizar la lista de la compra
+    document.getSelection();
+
+}

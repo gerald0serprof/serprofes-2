8171f3eee2a6039c4e6a7205fb196b1b43c02d14
gerald0serprof <gerald.lima.587@alumni.serprofes.es>
2026-05-18 21:55:24 +0200

    Added modo oscuro y el reto de desactivar un boton

--- ejercicios/260518-Fundamentos-DOM/script.js
+++ ejercicios/260518-Fundamentos-DOM/script.js
@@ -7,10 +7,12 @@ const spanContar = document.querySelector('#count');
 let contador = 0;
 
 //3. Escuchamos el evento clic en el botón
+/*
 btnContar.addEventListener('click', () => {
     contador++;
     spanContar.textContent = contador;
 });
+*/
 
 /* EJERCICIO 2 : Toggle Menu (Manipulación de Clases CSS) */
 const btnToggle = document.querySelector('#toggleMenu');
@@ -28,3 +30,45 @@ btnToggle.addEventListener('click', () => {
         btnToggle.textContent = 'Ocultar Menu';
     }
 });
+
+/* EJERCICIO 3 : Modo Oscuro */
+const toggleOscuro = document.querySelector('#themeToggle');
+const textoSwitch = document.querySelector('.switch-text');
+const cuerpoWeb = document.body;
+
+// Paso A -> Comprueba si el usuario ya tenia el modo oscuro guardado al acargar la web
+const temaGuardado = localStorage.getItem('temaPreferido')
+if (temaGuardado === 'oscuro') {
+    cuerpoWeb.classList.add('dark');
+    toggleOscuro.checked = true;
+    textoSwitch.textContent = 'Desactivar Modo Oscuro';
+}
+
+// Paso B -> Escucha cuadno el usuario marca o desmarca el checkbox
+toggleOscuro.addEventListener('change', () => {
+    if (toggleOscuro.checked) {
+        cuerpoWeb.classList.add('dark');
+        localStorage.setItem('temaPreferido', 'oscuro');
+        textoSwitch.textContent = 'Desactivar Modo Oscuro';
+    }else {
+        cuerpoWeb.classList.remove('dark');
+        localStorage.setItem('temaPreferido', 'claro');
+        textoSwitch.textContent = 'Activar Modo Oscuro';
+    }
+});
+
+/* RETO 1 : Desabilitar botón contador si llega a un limite X */
+const LIMITE_MAX = 10;
+const botonEspecial = document.getElementById('countBtn');
+
+btnContar.addEventListener('click', () => {
+    contador++;
+    spanContar.textContent = contador;
+    if (contador >= LIMITE_MAX) {
+        botonEspecial.disabled = true;
+        botonEspecial.style.backgroundColor = "gray";
+        botonEspecial.style.color = "white";
+        botonEspecial.style.cursor = "not-allowed";
+    }
+});
+

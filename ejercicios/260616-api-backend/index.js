ad3b0f788914cc32eb2aa103494c8e3e5a1ba6ca
gerald0serprof <gerald.lima.587@alumni.serprofes.es>
2026-06-18 19:39:43 +0200

    Added new exercise api-backend

---
+++ ejercicios/260616-api-backend/index.js
@@ -0,0 +1,54 @@
+//1. Importamos la herramienta principal (Express)
+const express = require("express");
+
+//2. Creamos nuestra aplicación(nuestro servidor)
+const app = express();
+
+//3. MIDDLEWARE (La línea mágica)
+// Esto es  un traductor. Le dice a Node: "Si alguien
+// te envia datos desde fuera, tradúcelos al formato
+// JSON para que podamos leerlos". Si falta esto
+// , el POST falla."
+app.use(express.json());
+
+//NUESTRA BASE DE DATOS
+// Guardamos información temporalmente en la lista array
+//dentro de la memoria del servidor
+let estudiantes = [
+    { id: 1, nombre: "Aroa", curso: "React"},
+    { id: 2, nombre: "Daniela", curso: "Node"},
+    { id: 3, nombre: "Jose", curso: "React"},
+    { id: 4, nombre: "Rafal", curso: "Node"},
+    { id: 5, nombre: "Pedro", curso: "React"},
+    { id: 6, nombre: "Ivan", curso: "Node"},
+    { id: 7, nombre: "Lewis", curso: "React"},
+    { id: 8, nombre: "Alexa", curso: "Node"},
+    { id: 9, nombre: "Bryan", curso: "React"},
+    { id: 10, nombre: "Rosa", curso: "Node"}
+];
+
+// RUTA GET: PARA LEER DATOS
+// Cuando alguien pregunte por "/api/estudiantes", el servidor muestra la lista
+app.get("/api/estudiantes", (req, res)=> {
+    res.json(estudiantes);
+});
+
+// RUTA POST: PARA GUARDAR DATOS NUEVOS
+//Cuando alguien envíe información a "api/estudiantes", hacemos lo siguiente
+app.post("/api/estudiantes", (req, res) => {
+    //A. Atrapamos los datos que vienen de fuera (viven dentro de req.body)
+    const nuevoEstudiante = req.body;
+    //B. Metemos ese estudiante nuevo en nuestra lista usando .push()
+    estudiantes.push(nuevoEstudiante);
+    //C. Le respondemos al usuario confirmando que todo ha ido bien
+    res.json({
+        mensaje: "¡Estudiante añadido con éxito a la base de datos!",
+        listaActualizada: estudiantes
+    });
+});
+
+//5.ENCENDER EL MOTOR
+// Le decimos al servidor que quede vigilando el puerto 3000
+app.listen(3000, () =>{
+    console.log("🎉¡Servidor funcionando! URL: http://localhost:3000");
+})
